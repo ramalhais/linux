3409341ddb40f12bea4d584e50cae61ece0ff62a
ramalhais <pedro@ramalhais.com>
2022-08-31 00:20:08 +0100

    build patch

--- build-NeXT.sh
+++ build-NeXT.sh
@@ -5,9 +5,10 @@ make next_defconfig
 make -j$[$(nproc)*2]
 DATE=$(date +%Y%m%d_%H%M%S)
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
-../simpkern vmlinux.binary_$DATE vmlinux.netimg_$DATE
+#../simpkern vmlinux.binary_$DATE vmlinux.netimg_$DATE
+#sudo cp vmlinux.netimg_$DATE /srv/tftp/
 ../../aout vmlinux.binary_$DATE vmlinux.netimg_aout_$DATE
-sudo cp vmlinux.netimg_$DATE /srv/tftp/
 sudo cp vmlinux.netimg_aout_$DATE /srv/tftp/
 sudo ln -sf vmlinux.netimg_aout_$DATE /srv/tftp/boot
+git diff v6.0-rc3 > ../linux-v6.0-rc3-NeXT-$(date +%F-%H.%M.%S).patch
 # m68k-linux-gnu-objdump -D vmlinux|less
