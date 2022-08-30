4586ef157e76f57c9593472c66893a6a34e8c184
ramalhais <pedro@ramalhais.com>
2022-08-30 22:27:20 +0100

    make next_defconfig

--- build-NeXT.sh
+++ build-NeXT.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 export ARCH=m68k CROSS_COMPILE=m68k-linux-gnu- GCC_SUFFIX=-9
 # make tinyconfig
+make next_defconfig
 make -j5
 DATE=$(date +%Y%m%d_%H%M%S)
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
