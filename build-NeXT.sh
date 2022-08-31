e106fa4156a37b110086ab9e6cc60e9896310428
ramalhais <pedro@ramalhais.com>
2022-08-31 01:01:17 +0100

    add NeXT tools

--- build-NeXT.sh
+++ build-NeXT.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 export ARCH=m68k CROSS_COMPILE=m68k-linux-gnu- GCC_SUFFIX=-9
 # make tinyconfig
+# make savedefconfig
+# cp defconfig arch/m68k/configs/next_defconfig
 make next_defconfig
 make -j$[$(nproc)*2]
 DATE=$(date +%Y%m%d_%H%M%S)
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
-#../simpkern vmlinux.binary_$DATE vmlinux.netimg_$DATE
+make -C arch/m68k/tools/next/
+#./arch/m68k/tools/next/simpkern vmlinux.binary_$DATE vmlinux.netimg_$DATE
 #sudo cp vmlinux.netimg_$DATE /srv/tftp/
-../../aout vmlinux.binary_$DATE vmlinux.netimg_aout_$DATE
+./arch/m68k/tools/next/aout vmlinux.binary_$DATE vmlinux.netimg_aout_$DATE
 sudo cp vmlinux.netimg_aout_$DATE /srv/tftp/
 sudo ln -sf vmlinux.netimg_aout_$DATE /srv/tftp/boot
 git diff v6.0-rc3 > ../linux-v6.0-rc3-NeXT-$(date +%F-%H.%M.%S).patch
