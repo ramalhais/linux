e67b55b4048216950fa14f3f2c91d2ec5968e95b
ramalhais <pedro@ramalhais.com>
2022-08-31 01:08:45 +0100

    improve build script

--- build-NeXT.sh
+++ build-NeXT.sh
@@ -1,17 +1,40 @@
 #!/bin/bash -x
-export ARCH=m68k CROSS_COMPILE=m68k-linux-gnu- GCC_SUFFIX=-9
+
+export ARCH=m68k
+export CROSS_COMPILE=m68k-linux-gnu-
+export GCC_SUFFIX=-9
+
+### Generate NeXT defconfig
 # make tinyconfig
 # make savedefconfig
 # cp defconfig arch/m68k/configs/next_defconfig
+
+### Setup NeXT config
 make next_defconfig
+
+### Build kernel
 make -j$[$(nproc)*2]
+
 DATE=$(date +%Y%m%d_%H%M%S)
+
+### Extract binary from ELF kernel image
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
+
+### Compile NeXT tools
 make -C arch/m68k/tools/next/
-#./arch/m68k/tools/next/simpkern vmlinux.binary_$DATE vmlinux.netimg_$DATE
-#sudo cp vmlinux.netimg_$DATE /srv/tftp/
+
+### Wrap kernel binary code in Mach-O header (bigger than aout (COFF?) header)
+#./arch/m68k/tools/next/simpkern vmlinux.binary_$DATE vmlinux.macho_$DATE
+#sudo cp vmlinux.macho_$DATE /srv/tftp/
+#sudo ln -sf vmlinux.macho_$DATE /srv/tftp/boot
+
+### Wrap kernel binary code in aout header (old UNIX COFF format?)
 ./arch/m68k/tools/next/aout vmlinux.binary_$DATE vmlinux.netimg_aout_$DATE
 sudo cp vmlinux.netimg_aout_$DATE /srv/tftp/
 sudo ln -sf vmlinux.netimg_aout_$DATE /srv/tftp/boot
+
+### Save patch
 git diff v6.0-rc3 > ../linux-v6.0-rc3-NeXT-$(date +%F-%H.%M.%S).patch
+
+### Show generated assembly code
 # m68k-linux-gnu-objdump -D vmlinux|less
