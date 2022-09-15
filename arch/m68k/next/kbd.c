a02dcbf0381a47218dbab9e49f49cbe1b4f8ec0b
ramalhais <pedro@ramalhais.com>
2022-09-15 05:59:34 +0100

    nextfb and next-kbd loading. nextfb panicking

--- arch/m68k/next/kbd.c
+++ arch/m68k/next/kbd.c
@@ -7,18 +7,26 @@
  *
  */
 
-#include <linux/types.h>
-#include <linux/kernel.h>
-#include <linux/kernel.h>
-#include <linux/mm.h>
+#include <linux/input.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+// #include <linux/types.h>
+// #include <linux/kernel.h>
+// #include <linux/kernel.h>
+// #include <linux/mm.h>
 
 #include <asm/nextints.h>
-#include <asm/nexthw.h>
-#include <asm/irq.h>
+// #include <asm/nexthw.h>
+// #include <asm/irq.h>
 
-#include <linux/kbd_ll.h>
+// #include <linux/kbd_ll.h>
 
-#include "nextmap.c"
+// #include "nextmap.c"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Pedro Ramalhais <ramalhais@gmail.com>");
+MODULE_DESCRIPTION("Keyboard driver for NeXT Computer/Cube/Station");
+MODULE_ALIAS("platform:next-keyboard");
 
 /* shouldn't be global i'll wager :) */
 struct mon {
@@ -65,32 +73,177 @@ typedef union {
 #define DEFAULT_KEYB_REP_DELAY  (HZ/4)
 #define DEFAULT_KEYB_REP_RATE   (HZ/25)
 
-/* this has to map the scan codes assigned in
-	nextmap.map 
-keycode 100 = Control
-keycode 101 = Shift
-keycode 102 = Shift
-keycode 103 = Alt
-keycode 104 = Alt
-	*/
-
-unsigned int flagscans[7]={
-	100,101,102,0,0,103,104
-};	
+#define NR_CTRL_KEYS 7
+// #define CTRL_BASE_CODE 81
+#define CTRL_BASE_CODE 100	// We may be missing some keys. Let's give it some room.
 
 static unsigned int oldflagmap=0;
 
-void next_kbd_int(int irq, void *dev_id, struct pt_regs *regs)
-{
-	__u32 csr=mon->csr;
-	__u32 data;
+struct next_kbd {
+	struct input_dev *input;
+	// void __iomem *addr;
+	// int irq;
+	// https://web.archive.org/web/20220131154708if_/http://xahlee.info/kbd/i/NeXT_keyboard_kTcqz.jpg
+	// https://web.archive.org/web/20220203021711if_/http://xahlee.info/kbd/i/NeXT_Computer_keyboard_66682.jpg
+	unsigned short keycodes[CTRL_BASE_CODE+6+1];
+};
 
+struct next_kbd tpl_next_kbd = {
+	.keycodes = {
+		[3] = KEY_BACKSLASH,
+		[4] = KEY_LEFTBRACE,
+		[5] = KEY_RIGHTBRACE,
+		[6] = KEY_I,
+		[7] = KEY_O,
+		[8] = KEY_P,
+		[9] = KEY_LEFT,
+		[11] = KEY_KP0,
+		[13] = KEY_KPDOT,
+		[13] = KEY_KPENTER,
+		[16] = KEY_RIGHT,
+		[17] = KEY_KP1,
+		[18] = KEY_KP4,
+		[19] = KEY_KP6,
+		[20] = KEY_KP3,
+		[21] = KEY_KPPLUS,
+		[22] = KEY_UP,
+		[23] = KEY_KP2,
+		[24] = KEY_KP5,
+		[27] = KEY_DELETE,
+		[28] = KEY_EQUAL,
+		[29] = KEY_MINUS,
+		[30] = KEY_8,
+		[31] = KEY_9,
+		[32] = KEY_0,
+		[33] = KEY_KP7,
+		[34] = KEY_KP8,
+		[35] = KEY_KP9,
+		[36] = KEY_KPMINUS,
+		[37] = KEY_KPASTERISK,
+		[38] = KEY_GRAVE,
+		[39] = KEY_EQUAL,
+		[40] = KEY_KPSLASH,
+		[42] = KEY_ENTER,
+		[43] = KEY_APOSTROPHE,
+		[44] = KEY_SEMICOLON,
+		[45] = KEY_L,
+		[46] = KEY_COMMA,
+		[47] = KEY_DOT,
+		[48] = KEY_SLASH,
+		[49] = KEY_Z,
+		[50] = KEY_X,
+		[51] = KEY_C,
+		[52] = KEY_V,
+		[53] = KEY_B,
+		[54] = KEY_M,
+		[55] = KEY_N,
+		[56] = KEY_SPACE,
+		[57] = KEY_A,
+		[58] = KEY_S,
+		[59] = KEY_D,
+		[60] = KEY_F,
+		[61] = KEY_G,
+		[62] = KEY_K,
+		[63] = KEY_J,
+		[64] = KEY_H,
+		[65] = KEY_TAB,
+		[66] = KEY_Q,
+		[67] = KEY_W,
+		[68] = KEY_E,
+		[69] = KEY_R,
+		[70] = KEY_U,
+		[71] = KEY_Y,
+		[72] = KEY_T,
+		[73] = KEY_ESC,
+		[74] = KEY_1,
+		[75] = KEY_2,
+		[76] = KEY_3,
+		[77] = KEY_4,
+		[78] = KEY_7,
+		[79] = KEY_6,
+		[80] = KEY_5,
+		[CTRL_BASE_CODE]	= KEY_LEFTCTRL,
+		[CTRL_BASE_CODE+1]	= KEY_LEFTSHIFT,
+		[CTRL_BASE_CODE+2]	= KEY_RIGHTSHIFT,
+		[CTRL_BASE_CODE+3]	= KEY_LEFTMETA,
+		[CTRL_BASE_CODE+4]	= KEY_RIGHTMETA,
+		[CTRL_BASE_CODE+5]	= KEY_LEFTALT,
+		[CTRL_BASE_CODE+6]	= KEY_RIGHTALT
+	}
+};
+
+// void next_kbd_int(int irq, void *dev_id, struct pt_regs *regs)
+// {
+// 	__u32 csr=mon->csr;
+// 	__u32 data;
+
+// 	unsigned int mask,index,changed,scan;
+	
+// 	/* ack the int */
+// 	mon->csr=(csr & ~KM_INT);
+
+// 	if(!(csr & KM_HAVEDATA)) return;
+
+// 	data=mon->km_data;
+
+// 	if((data & KD_ADDRMASK) == KD_KADDR) {
+// 		/* keyboard reporting */
+
+
+// 		/* the next only has a bitmap of which ctl keys
+// 		are currently down, so we just triggers ups and downs
+// 		to match how we last thought the world was. We do this
+// 		first so real keys pressed after we get out of sync
+// 		don't suffer. */
+
+// 		changed=oldflagmap;
+// 		oldflagmap=data & KD_FLAGKEYS;
+
+// 		if((changed^=oldflagmap)) {
+// 			/* use cool bitmap instructions */
+// 			for(mask=KD_CNTL,index=0;index<7;mask<<=1,index++) {
+	
+// 				if(!(changed&mask)) continue;
+// 				if(!(scan=flagscans[index])) continue;
+// 				handle_scancode(scan | (data&mask ? 0: 0x80));
+
+// 			}
+// 		}
+// 		if(data & KD_VALID) {  
+// 			/* a 'real' key has changed */
+// 			/* luckly the next and kernel use the same 'up/down' flag :) */
+// 			handle_scancode(data&(KD_KEYMASK|KD_DIRECTION));
+// 		}
+				
+// 	} 
+// }
+
+static irqreturn_t next_kbd_int(int irq, void *dev_id)
+{
+	unsigned long flags;
+	struct next_kbd *kbd = dev_id;
+	struct input_dev *input = kbd->input;
+	__u32 csr;
+	__u32 data;
 	unsigned int mask,index,changed,scan;
+
+	local_irq_save(flags);
+
+	if (!next_irq_pending(NEXT_IRQ_KYBD_MOUSE)) {
+		printk("Interrupt 3 not for NeXT keyboard/mouse\n");
+		local_irq_restore(flags);
+		return IRQ_NONE;
+	}
+
 	
 	/* ack the int */
 	mon->csr=(csr & ~KM_INT);
+	csr = mon->csr;
 
-	if(!(csr & KM_HAVEDATA)) return;
+	if(!(csr & KM_HAVEDATA)) {
+		printk("NeXT keyboard/mouse interrupt, but no data to handle\n");
+		return IRQ_HANDLED;
+	}
 
 	data=mon->km_data;
 
@@ -109,39 +262,112 @@ void next_kbd_int(int irq, void *dev_id, struct pt_regs *regs)
 
 		if((changed^=oldflagmap)) {
 			/* use cool bitmap instructions */
-			for(mask=KD_CNTL,index=0;index<7;mask<<=1,index++) {
-	
-				if(!(changed&mask)) continue;
-				if(!(scan=flagscans[index])) continue;
-				handle_scancode(scan | (data&mask ? 0: 0x80));
-
+			for(mask=KD_CNTL,index=0; index<NR_CTRL_KEYS; mask<<=1,index++) {	
+				if (!(changed&mask))
+					continue;
+				scan = CTRL_BASE_CODE+index;
+				input_report_key(input, scan, !!(data&mask));
+				input_sync(input);
 			}
 		}
 		if(data & KD_VALID) {  
 			/* a 'real' key has changed */
-			/* luckly the next and kernel use the same 'up/down' flag :) */
-			handle_scancode(data&(KD_KEYMASK|KD_DIRECTION));
+			input_report_key(input, data&KD_KEYMASK, !!(data&KD_DIRECTION));
+			input_sync(input);
 		}
 				
 	} 
+
+	local_irq_restore(flags);
+
+	return IRQ_HANDLED;
 }
 
-int next_kbd_init(void) {
+static int next_kbd_probe(struct platform_device *pdev) {
+	struct input_dev *input;
+	struct next_kbd *next_kbd;
+	int ret;
 
-/*        plain_map, shift_map, altgr_map, 0,
-        ctrl_map, shift_ctrl_map, 0, 0,
-        alt_map, 0, 0, 0,
-        ctrl_alt_map,   0 */
+	*(volatile unsigned char *)(0xff110000)=0xF4;
+	input = input_allocate_device();
+	if (!input) {
+		dev_err(&pdev->dev, "Unable to allocate input device for NeXT Keyboard\n");
+		return -ENOMEM;
+	}
 
-	memcpy(key_maps[0], nextplain_map, sizeof(plain_map));
-	memcpy(key_maps[1], nextshift_map, sizeof(plain_map));
-	memcpy(key_maps[2], nextaltgr_map, sizeof(plain_map));
-	memcpy(key_maps[4], nextctrl_map, sizeof(plain_map));
-	memcpy(key_maps[5], nextshift_ctrl_map, sizeof(plain_map));
-	memcpy(key_maps[8], nextalt_map, sizeof(plain_map));
-	memcpy(key_maps[12], nextctrl_alt_map, sizeof(plain_map));
+	next_kbd = devm_kzalloc(&pdev->dev, sizeof(*next_kbd), GFP_KERNEL);
+	if (!next_kbd) {
+		return -ENOMEM;
+	}
+	*(volatile unsigned char *)(0xff110000)=0xF5;
 
-	request_irq(NEXT_IRQ_KYBD_MOUSE, next_kbd_int, IRQ_FLG_LOCK, "keyboard/mouse", next_kbd_int);
-	return 1;
+	next_kbd->input = input;
+
+	input->name = pdev->name;
+	input->phys = "next-kbd/input0";
+
+	input->id.bustype = BUS_HOST;
+	input->id.vendor = 0x000F;
+	input->id.product = 0x0001;
+	input->id.version = 0x0100;
+
+	input->keycode = next_kbd->keycodes;
+	input->keycodesize = sizeof(next_kbd->keycodes[0]);
+	input->keycodemax = ARRAY_SIZE(next_kbd->keycodes);
+
+	for (int i = 0; i < ARRAY_SIZE(next_kbd->keycodes); i++) {
+		next_kbd->keycodes[i] = tpl_next_kbd.keycodes[i];
+		if (next_kbd->keycodes[i] != 0) {
+			__set_bit(next_kbd->keycodes[i], input->keybit);
+		}
+	}
+	// __clear_bit(KEY_RESERVED, input->keybit);
+
+	// __set_bit(EV_KEY, input->evbit);
+
+	ret = input_register_device(input);
+	if (ret) {
+		dev_err(&pdev->dev, "unable to register input device\n");
+		return ret;
+	}
+
+	if (request_irq(IRQ_AUTO_3, next_kbd_int, IRQF_SHARED, "NeXT keyboard/mouse", next_kbd_int)) {
+	// *(volatile unsigned char *)(0xff110000)=0x99; // Previous debug
+		pr_err("Failed to register NeXT keyboard/mouse interrupt\n");
+	}
+
+	next_intmask_enable(NEXT_IRQ_KYBD_MOUSE-NEXT_IRQ_BASE);
+
+	return 0;
 }
 
+static struct platform_driver next_kbd_driver = {
+	.probe    = next_kbd_probe,
+	.driver   = {
+		.name = "next-kbd",
+	},
+};
+
+// module_platform_driver(next_kbd_pd);
+// module_platform_driver_probe(next_kbd_pd, next_kbd_probe);
+
+
+static struct platform_device next_kbd_device = {
+	.name	= "next-kbd",
+};
+
+static int next_kbd_init(void)
+{
+	int ret = 0;
+
+	ret = platform_driver_register(&next_kbd_driver);
+
+	if (!ret) {
+		ret = platform_device_register(&next_kbd_device);
+		if (ret)
+			platform_driver_unregister(&next_kbd_driver);
+	}
+	return ret;
+}
+
+module_init(next_kbd_init);
