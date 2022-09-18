81c675cd47a24f5ccfb875953e9d870d5688111f
ramalhais <pedro@ramalhais.com>
2022-09-18 04:05:33 +0100

    nextfb framebuffer works on NeXT Color. fix keyboard. cleanup

--- arch/m68k/next/config.c
+++ arch/m68k/next/config.c
@@ -2,34 +2,18 @@
 /*
  *  linux/arch/m68k/next/config.c
  *
+ *  Copyright (C) 1998 Zach Brown <zab@zabbo.net>
+ *  Copyright (C) 2022 Pedro Ramalhais <ramalhais@gmail.com>
+ *
  *  This file contains the NeXT-specific initialisation code.  It gets
  *  called by setup_mm.c .
  */
 
-//#include <linux/module.h>
-//#include <linux/string.h>
-//#include <linux/rtc.h>
-
-//#include <linux/config.h>
-//#include <linux/types.h>
 #include <linux/kernel.h>
-//#include <linux/mm.h>
-//#include <linux/kd.h>
-//#include <linux/tty.h>
 #include <linux/console.h>
 #include <linux/init.h>
-
-//#include <asm/byteorder.h>
-//#include <asm/blinken.h>
-//#include <asm/io.h>                               /* readb() and writeb() */
-//#include <asm/config.h>
-
 #include <asm/bootinfo.h>
 #include <asm/bootinfo-next.h>
-//#include <asm/setup.h>
-//#include <asm/system.h>
-//#include <asm/pgtable.h>
-//#include <asm/irq.h>
 #include <asm/machdep.h>
 
 #include <asm/nextints.h> 
@@ -41,18 +25,6 @@ struct prom_info prom_info;
 /* yanked from the eprom chip */
 struct eprom_info eprom_info;
 
-extern int next_kbd_init(void);
-extern int next_get_irq_list(char *buf);
-// extern void next_clock_init(void);
-// extern void next_gettod(int *yearp, int *monp, int *dayp, int *hourp, int *minp, int *secp);
-// extern unsigned long next_gettimeoffset(void);
-// extern void rtc_init(void);
-
-// void __init next_sched_init(void)
-// {
-//        next_clock_init();
-// }
-
 void next_get_model(char *model) {
        strcpy(model,"Generic NeXT");
 }
@@ -63,9 +35,8 @@ void __init next_meminit(void) {
 
 	m68k_num_memory=0;
 	for(i=0;i<4;i++){
-		printk("SIMM bank %d: ",i);
 		if(prom_info.simm_info[i].start==0) {
-			printk("unused\n");
+			printk("SIMM bank %d: Empty", i);
 			continue;
 		}
 		m68k_memory[m68k_num_memory].addr=prom_info.simm_info[i].start;
@@ -75,66 +46,41 @@ void __init next_meminit(void) {
 		* the end addr sometimes doesn't include the
 		* last page 
 		*/
+		// (PR) The NeXT PROM sets up data at the end of the last used memory bank and reserves that. It's OK, we can reuse it.
 
 		len=prom_info.simm_info[i].end-prom_info.simm_info[i].start;
 		if(len&0xfffff) len+=0x100000-(len&0xfffff); /* silly */
 		m68k_memory[m68k_num_memory].size=len;
 		
-		printk("%2lxMB at 0x%0lx\n",
-			m68k_memory[m68k_num_memory].size>>20,
+		printk("SIMM bank %d: %d MB at 0x%0lx\n",
+			i,
+			(int)m68k_memory[m68k_num_memory].size>>20,
 			m68k_memory[m68k_num_memory].addr);
 
 		m68k_num_memory++;
 	}
 }
 
-/*
-#ifdef CONFIG_SERIAL_8250_CONSOLE
-extern int next_setup_serial_console(void) __init;
-#endif
-*/
-
 void __init next_init_IRQ(void)
 {
-	// Disable NeXT interrupts
-	// (*((volatile u_int *)NEXT_INTMASK)) = 0;
+	// Disable all interrupts on NeXT IRQ controller
 	next_get_intmask() = 0;
-	// next_intmask_enable(NEXT_IRQ_NMI-NEXT_IRQ_BASE);
-	// next_intmask_enable(NEXT_IRQ_PFAIL-NEXT_IRQ_BASE);
-	// next_intmask_enable(NEXT_IRQ_BUS-NEXT_IRQ_BASE);
-	// next_intmask_enable(NEXT_IRQ_POWER-NEXT_IRQ_BASE);
-	// next_intmask_enable(NEXT_IRQ_SOFTINT1-NEXT_IRQ_BASE);
-	// next_intmask_enable(NEXT_IRQ_SOFTINT0-NEXT_IRQ_BASE);
 }
 
 void __init config_next(void)
 {
 	// *(volatile unsigned char *)(0xff110000)=0x7A;
 	next_meminit();
-	// next_kbd_init();
 
 	mach_sched_init		= next_sched_init;
 	mach_init_IRQ		= next_init_IRQ;
 	mach_get_model		= next_get_model;
-//	mach_request_irq	= next_request_irq;
-//	mach_free_irq		= next_free_irq;
-//	enable_irq			= next_enable_irq;
-//	disable_irq			= next_disable_irq;
-//	mach_process_int	= next_process_int;
-//	mach_default_handler	= &next_default_handlers;
-
-
-	// mach_gettimeoffset	= next_gettimeoffset; // not used anymore?
-	// mach_gettod			= next_gettod; // not used anymore?
-//	mach_get_irq_list 	= next_get_irq_list;
-
-
 //void (*mach_get_hardware_list) (struct seq_file *m);
 //unsigned int (*mach_get_ss)(void);
 //void (*mach_reset)( void );
 //void (*mach_halt)( void );
 //void (*mach_power_off)( void );
-//#ifdef CONFIG_HEARTBEAT
+//#ifdef CONFIG_HEARTBEAT // Example: LED as heartbeat
 //void (*mach_heartbeat) (int);
 //#ifdef CONFIG_M68K_L2_CACHE
 //void (*mach_l2_flush) (int);
@@ -147,15 +93,9 @@ void __init config_next(void)
 //extern void (*mach_beep) (unsigned int, unsigned int);
 
 
-#ifdef CONFIG_DUMMY_CONSOLE
-	conswitchp          = &dummy_con;
-#endif
+// #ifdef CONFIG_DUMMY_CONSOLE
+// 	conswitchp          = &dummy_con;
+// #endif
 
 //	mach_max_dma_address = 0xffffffff;
-
-/*
-#ifdef CONFIG_NEXT_CONSOLE
-	next_setup_serial_console();
-#endif
-*/
 }
