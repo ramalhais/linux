72f676b9cb0496f361f838daa88f9888bb8c41a1
ramalhais <pedro@ramalhais.com>
2022-09-18 04:05:33 +0100

    nextfb framebuffer works on NeXT Color. fix keyboard. cleanup

---
+++ arch/m68k/tools/next/initramfs/init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for ALIAS in $(busybox --list); do
+  if [ "$ALIAS" != "busybox" ] && [ "$ALIAS" != "sh" ]; then
+  	ln -s busybox "/bin/$ALIAS"
+  fi
+done
+mount -t devtmpfs none /dev
+mount -t proc none /proc
+mount -t sysfs none /sys
+setsid sh -c 'exec sh </dev/tty1 >/dev/tty1 2>&1'
