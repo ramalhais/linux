000cb53b014bf3b30a79312d32c462a08ee92fbc
Pedro Ramalhais <pedro@ramalhais.com>
2022-09-24 18:12:11 +0100

    NeXT: improve github workflow

--- build-NeXT-github.sh
+++ build-NeXT-github.sh
@@ -6,13 +6,13 @@ export GCC_SUFFIX=-10
 
 make -j$[$(nproc)*2] || exit 1
 
-DATE=$(date +%F-%H.%M.%S)
-KERNELVER=$(make kernelversion)
+# DATE=$(date +%F-%H.%M.%S)
+# KERNELVER=$(make kernelversion)
 
 ### Extract binary from ELF kernel image
-m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
+m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary
 
 ### Compile NeXT tools
 make -C arch/m68k/tools/next/
 
-./arch/m68k/tools/next/aout vmlinux.binary_$DATE vmlinux-$KERNELVER-NeXT_$DATE
+./arch/m68k/tools/next/aout vmlinux.binary vmlinux-NeXT
