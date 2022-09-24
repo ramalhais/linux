173d896a1a2e1d4a9d902188b639494b2995ccf0
Pedro Ramalhais <pedro@ramalhais.com>
2022-09-24 17:41:49 +0100

    NeXT: add github workflow

---
+++ build-NeXT-github.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+export ARCH=m68k
+export CROSS_COMPILE=m68k-linux-gnu-
+export GCC_SUFFIX=-10
+
+make -j$[$(nproc)*2] || exit 1
+
+DATE=$(date +%F-%H.%M.%S)
+KERNELVER=$(make kernelversion)
+
+### Extract binary from ELF kernel image
+m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary_$DATE
+
+### Compile NeXT tools
+make -C arch/m68k/tools/next/
+
+./arch/m68k/tools/next/aout vmlinux.binary_$DATE vmlinux-$(KERNELVER)-NeXT_$DATE
