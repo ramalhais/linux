b42c0f50aa634cb9f70e541fd119133cb9f01b39
Pedro Ramalhais <pedro@ramalhais.com>
2022-10-17 23:58:38 +0100

    NeXT: veth setup

---
+++ arch/m68k/tools/next/tftpboot/setup-veth.sh
@@ -0,0 +1,27 @@
+# Configuration
+HOST_IF=ens33
+HOST_VIF=veth0
+HIST_VIF_IP=192.168.111.1
+GUEST_VIF=veth0guest
+
+
+# Configure networking
+ip link add $HOST_VIF type veth peer $GUEST_VIF
+ethtool --offload $HOST_VIF tx off
+ip addr add $HIST_VIF_IP/24 dev $HOST_VIF
+ip link set $HIST_VIF up
+ip link set $GUEST_VIF up
+
+# NeXT PROM doesn't do ARP. Match these to dhcpd.conf.
+arp -s 192.168.111.12 00:00:0f:00:30:90
+arp -s 192.168.111.13 00:00:0f:00:f3:02
+arp -s 192.168.111.14 00:00:0f:12:34:56
+
+# Restart TFTPd and DHCPd
+systemctl restart tftpd-hpa
+systemctl restart isc-dhcp-server
+
+# Forwarding/SNAT
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -I POSTROUTING -o $HOST_IF -j MASQUERADE
+
