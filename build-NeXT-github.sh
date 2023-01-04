7af504f787824a2f0d5e110a712c8d4484599f05
Pedro Ramalhais <pedro@ramalhais.com>
2023-01-04 22:23:21 +0000

    upload aout and macho

--- build-NeXT-github.sh
+++ build-NeXT-github.sh
@@ -1,10 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash -x -e
 
 export ARCH=m68k
 export CROSS_COMPILE=m68k-linux-gnu-
 export GCC_SUFFIX=-10
 
-make next_fb_net_nfs_iscsi_fs_scsi_part_defconfig && make -j$[$(nproc)*2] || exit 1
+NPROCS=$[$(nproc)*2]
+
+make next_fb_net_nfs_iscsi_fs_scsi_part_defconfig
+make -j$NPROCS
 
 # DATE=$(date +%F-%H.%M.%S)
 # KERNELVER=$(make kernelversion)
@@ -13,7 +16,7 @@ make next_fb_net_nfs_iscsi_fs_scsi_part_defconfig && make -j$[$(nproc)*2] || exi
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary
 
 ### Compile NeXT tools
-make -C arch/m68k/tools/next/
+make -j$NPROCS -C arch/m68k/tools/next/
 
 ./arch/m68k/tools/next/aout vmlinux.binary vmlinux-NeXT.aout
 ./arch/m68k/tools/next/simpkern vmlinux.binary vmlinux-NeXT.macho-simpkern
