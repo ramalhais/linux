0a9de83359a59a3cb4b715024e3621ecc5faf68b
Pedro Ramalhais <pedro@ramalhais.com>
2023-01-07 06:15:07 +0000

    next-disklabel add support for creating disklabel. improve build

--- build-NeXT-github.sh
+++ build-NeXT-github.sh
@@ -7,17 +7,24 @@ export GCC_SUFFIX=-10
 
 NPROCS=$[$(nproc)*2]
 
+# Compile NeXT tools
+make -j$NPROCS -C arch/m68k/tools/next/
+
+# Get netbsd bootloader
+curl -v -L -o arch/m68k/tools/next/netbsd-boot-next.aout https://github.com/ramalhais/netbsd-src/releases/latest/download/netbsd-boot-next.aout
+
+# Compile Kernel
 make next_fb_net_nfs_iscsi_fs_scsi_part_defconfig
 make -j$NPROCS
 
 # DATE=$(date +%F-%H.%M.%S)
 # KERNELVER=$(make kernelversion)
 
+### Strip symbols
+m68k-linux-gnu-strip --strip-unneeded vmlinux -o vmlinux.stripped
+
 ### Extract binary from ELF kernel image
 m68k-linux-gnu-objcopy --output-target=binary vmlinux vmlinux.binary
-
-### Compile NeXT tools
-make -j$NPROCS -C arch/m68k/tools/next/
 
 ./arch/m68k/tools/next/aout vmlinux.binary vmlinux-NeXT.aout
 ./arch/m68k/tools/next/simpkern vmlinux.binary vmlinux-NeXT.macho-simpkern
