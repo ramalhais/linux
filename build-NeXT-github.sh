2fe22fa6c1c0bca42dd881dfc002d2a99e9ce5fc
Pedro Ramalhais <pedro@ramalhais.com>
2023-01-07 07:37:01 +0000

    fix m68k gcc

--- build-NeXT-github.sh
+++ build-NeXT-github.sh
@@ -10,8 +10,7 @@ NPROCS=$[$(nproc)*2]
 # Compile NeXT tools for m68k
 make CC=${CROSS_COMPILE}gcc${GCC_SUFFIX} -j$NPROCS -C arch/m68k/tools/next/
 for BIN in aout macho next-disklabel; do
-	cp arch/m68k/tools/next/$BIN arch/m68k/tools/next/$BIN.m68k
-	rm arch/m68k/tools/next/$BIN
+	mv arch/m68k/tools/next/$BIN arch/m68k/tools/next/$BIN.m68k
 done
 
 # Compile NeXT tools for amd64
